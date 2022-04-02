e5af33a05523d9de4884b720083080d038c953e0
prikachikovim <prikazchikovim@63pokupki.ru>
2022-04-03 02:00:13 +0400

    [feat] add socket.io connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(3000, () => {
+io.on('connection', () => {
+  console.log('New Websocket connection');
+})
+
+server.listen(3000, () => {
   console.log("The server is up on port", port);
 });
